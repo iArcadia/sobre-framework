4bcf1e3e2bc6c6d140217fff192347f2d2a681ce
kbibollet <bibollet.kevin@gmail.com>
2020-01-29 11:45:30 +0100

    Initial commit

--- commands/composer/update.php
+++ commands/composer/update.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../../../vendor/autoload.php');
 
 exec('composer require sobre-framework/core ' . $argv[1], $output);
 

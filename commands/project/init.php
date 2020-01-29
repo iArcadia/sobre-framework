373a6d61088f78cb3bc6b511e31117759c4946d7
kbibollet <bibollet.kevin@gmail.com>
2020-01-29 11:48:46 +0100

    Initial commit

--- commands/project/init.php
+++ commands/project/init.php
@@ -6,7 +6,7 @@ $dir_to_copy = ['public', 'database', 'config'];
 $root_files_to_copy = ['index.php', '.env', '.htaccess'];
 
 foreach ($root_files_to_copy as $file) {
-    copy(sobre_path($file), root_path($file));
+    copy(sobre_path('core/' . $file), root_path($file));
 }
 
 echo 'The Sobre Framework project has been correctly initialized.';

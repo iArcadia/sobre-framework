1c93b77e3eb6eb0372d2edbf94b8b56589ea9084
kbibollet <bibollet.kevin@gmail.com>
2020-01-29 15:42:44 +0100

    Initial commit

--- src/Database.php
+++ src/Database.php
@@ -36,9 +36,9 @@ class Database
 
             $this->getPdo()->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $this->getPdo()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            return $this;
         }
+
+        return $this;
     }
 
     /**
